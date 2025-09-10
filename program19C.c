304139c0ee3be29980417d3a0ed8ae61b7ab1160
vishv-vaghasiya <vishvaghasiya02@gmail.com>
2025-09-10 21:55:15 +0530

    Add files via upload

---
+++ program19C.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+main()
+{
+    int i,j,k;
+    int n=5;
+
+    for(i=n;i>=1;i--)
+    {
+
+        for(j=0;j<n-i;j++)
+        {
+            printf("  ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        for(j=2;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    for(i=2;i<=n;i++)
+    {
+        for(j=0;j<n-i;j++)
+        {
+            printf("  ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        for(j=2;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
+
