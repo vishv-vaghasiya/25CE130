905e33d88fc9171134665208276f79abb3a657a9
vishv-vaghasiya <vishvaghasiya02@gmail.com>
2025-09-10 21:52:08 +0530

    Add files via upload

---
+++ program13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+    int x;
+
+    for(x=10;x<=100;x=x+10)
+    {
+        printf("CURRENT WATER LEVEL : %d LITERS",x);
+
+        if(x%100==0)
+        {
+            printf("(TANK IS FULL)");
+        }
+
+        printf("\n");
+    }
+}
